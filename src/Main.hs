87f90f5d7f303f2ee917df0a002ae2b9e1c5aea3
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2015-10-01 17:55:06 +0200

    MAKE IT WEBSCALE

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,16 @@ module Main where
 
 import Nouns
 
+import Control.Concurrent
+import Data.Char
+import Data.Maybe
 import System.Environment
 import System.Exit
 import System.Random
-import Data.Char
-import Data.Maybe
+import System.IO
+import Network
+
+port_number = 5002
 
 lastN :: Int -> [a] -> [a]
 lastN n xs = foldl (const . drop 1) xs (drop n xs)
@@ -32,10 +37,28 @@ berrify str = case lastN 5 str of
 capitalize :: String -> String
 capitalize (x:xs) = (toUpper x):xs
 
-
 main :: IO ()
-main = do
-    args <- getArgs
-    name <- generateName $ getFirstLetter args
-    adjective <- getRandomElement adjectives
-    putStrLn ((capitalize adjective) ++ " " ++ (capitalize (berrify name)))
+main = withSocketsDo $ do
+    sock <- listenOn $ PortNumber port_number
+    let port = show port_number
+    putStrLn ("Listening on port " ++ port ++ ": http://127.0.0.1:" ++ port)
+    loop sock
+
+loop :: Socket -> IO ()
+loop sock = do
+   (h,_,_) <- accept sock
+   forkIO $ body h
+   loop sock
+   where
+       body h = do
+           name <- generateName $ getFirstLetter []
+           adjective <- getRandomElement adjectives
+           let berry = ((capitalize adjective) ++ " " ++ (capitalize (berrify name)))
+           hPutStr h $ msg berry
+           hFlush h
+           hClose h
+
+msg :: String -> String
+msg str = "HTTP/1.0 200 OK\r\nContent-Length: " ++ len ++ "\r\n\r\n" ++ str ++ "\r\n"
+    where
+        len = show $ length str
